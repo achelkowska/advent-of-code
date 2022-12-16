257d687b29d01450fbdb1acd1a467ef23f038051
achelkowska <agn.chelkowska@gmail.com>
2022-12-16 23:08:19 +0100

    DAY 9 solution - complete

--- DAY-9/day-9.py
+++ DAY-9/day-9.py
@@ -1,119 +1,54 @@
-
-H = [0,0]
-T = [0,0]
-
-
-def check_HT_distance(tail, head):
-    if head == tail:
-        return "s"
-    elif head[0] == tail[0] and abs(head[1]-tail[1]) <= 1:
-        return "lr"
-    elif head[1] == tail[1] and abs(head[0]-tail[0]) <= 1:
-        return("ud")
-    elif abs(head[1]-tail[1]) <= 1 and abs(head[0]-tail[0]) <= 1:
-        return("c")
+def check_T_position(coord_a, coord_b):
+    #return true if points are touching
+    if abs(coord_a[0]-coord_b[0]) <= 1 and abs(coord_a[1] - coord_b[1]) <= 1:
+        return True
     else:
-        return("shift")
+        return False
 
-def move_tail(tail, pos, head):
-    if pos == "s":
-        pass
-    elif pos == "lr":
-        if abs(head[1]-tail[1]+1) < abs(head[1]-tail[1]-1):
-            return [tail[0], tail[1]-1]
-        else:
-            return [tail[0], tail[1]+1]
-    elif pos == "ud":
-        if abs(head[0]-tail[0]+1) < abs(head[0]-tail[0]-1):
-            return [tail[0]-1, tail[1]]
-        else:
-            return [tail[0]+1, tail[1]]
-    elif pos == "c":
-        if abs(head[1]-tail[1]+1) < abs(head[1]-tail[1]-1):
-            if abs(head[0]-tail[0]+1) < abs(head[0]-tail[0]-1): #left-down
-                return [tail[0]-1, tail[1]-1]
-            else: #left-top
-                return [tail[0]+1, tail[1]-1]
-        else:
-            if abs(head[0]-tail[0]+1) < abs(head[0]-tail[0]-1): #right-down
-                return [tail[0]-1, tail[1]+1]
-            else: # right-top
-                return [tail[0]+1, tail[1]+1]
-    else:
-        print("else: ", head, tail, pos)
-    
-tail_positions_final = [[0,0]]
-temp_positions = []
-with open("input.txt") as inpt:
-    for i in inpt:
-        tab = i.strip().split()
-        if tab[0] == "R": #move head to right
-            for _ in range(0,int(tab[1])):
-                H[1] += 1
-                pos = check_HT_distance(T, H)
-                temp_positions.append(pos)
-                if pos != "shift":
-                    pass
-                else:
-                    for p in reversed(temp_positions):
-                        if p != "shift":
-                            pos = p
-                            break
-                    T = move_tail(T, pos, H)
-                    pos = check_HT_distance(T, H)
-                    temp_positions.append(pos)
-                    if T not in tail_positions_final:
-                        tail_positions_final.append(T)
-        elif tab[0] == "U": #move head to up
-            for _ in range(0,int(tab[1])):
-                H[0] += 1
-                pos = check_HT_distance(T, H)
-                temp_positions.append(pos)
-                if pos != "shift":
-                    pass
-                else:
-                    for p in reversed(temp_positions):
-                        if p != "shift":
-                            mv = p
-                            break
-                    T = move_tail(T, mv, H)
-                    pos = check_HT_distance(T, H)
-                    temp_positions.append(pos)
-                    if T not in tail_positions_final:
-                        tail_positions_final.append(T)
-        elif tab[0] == "L": #move head to left
-            for _ in range(0,int(tab[1])):
-                H[1] -= 1
-                pos = check_HT_distance(T, H)
-                temp_positions.append(pos)
-                if pos != "shift":
-                    pass
-                else:
-                    for p in reversed(temp_positions):
-                        if p != "shift":
-                            mv = p
-                            break
-                    T = move_tail(T, mv, H)
-                    pos = check_HT_distance(T, H)
-                    temp_positions.append(pos)
-                    if T not in tail_positions_final:
-                        tail_positions_final.append(T)
-        elif tab[0] == "D": #move head to down
-            for _ in range(0,int(tab[1])):
-                H[0] -= 1
-                pos = check_HT_distance(T, H)
-                temp_positions.append(pos)
-                if pos != "shift":
-                    pass
-                else:
-                    for p in reversed(temp_positions):
-                        if p != "shift":
-                            mv = p
-                            break
-                    T = move_tail(T, mv, H)
-                    pos = check_HT_distance(T, H)
-                    temp_positions.append(pos)
-                    if T not in tail_positions_final:
-                        tail_positions_final.append(T)
+def shift_point(coord_a, coord_b):
+    if coord_a[0] == coord_b[0]: #move left/right
+        if coord_a[1] > coord_b[1]: #right
+            return [coord_b[0], coord_b[1]+1]
+        elif coord_a[1] < coord_b[1]: #left
+            return [coord_b[0], coord_b[1]-1]
+    elif coord_a[1] == coord_b[1]: #move up/down
+        if coord_a[0] > coord_b[0]: #up
+            return [coord_b[0]+1, coord_b[1]]
+        if coord_a[0] < coord_b[0]: #down
+            return [coord_b[0]-1, coord_b[1]]
+    else: # move diagonal
+        if coord_a[0] > coord_b[0] and coord_a[1] > coord_b[1]: #up/right
+            return [coord_b[0]+1, coord_b[1]+1]
+        elif coord_a[0] < coord_b[0] and coord_a[1] > coord_b[1]: #down/right
+            return [coord_b[0]-1, coord_b[1]+1]
+        elif coord_a[0] > coord_b[0] and coord_a[1] < coord_b[1]: #up/left
+            return [coord_b[0]+1, coord_b[1]-1]
+        elif coord_a[0] < coord_b[0] and coord_a[1] < coord_b[1]: #down/left
+            return [coord_b[0]-1, coord_b[1]-1]
 
-print("Part 1: ", len(tail_positions_final))
+
+## coords: 1 - ud, 2 - lr
+
+def run_calc(nodes):
+    directions = {"R": (0,1), "L":(0,-1), "U":(1,0), "D":(-1,0)}
+    point_coords = [[0,0] for _ in range(nodes)] # 1st is head, and 1-9 tails
+    nine_coords = [(0,0)]
+
+    with open("input.txt") as inpt:
+        for i in inpt:
+            tab = i.strip().split()
+            for moves in range(int(tab[1])):
+                point_coords[0][0] += directions[tab[0]][0]
+                point_coords[0][1] += directions[tab[0]][1]
+
+                for t in range(1, len(point_coords)):
+                    adjacent = check_T_position(point_coords[t-1], point_coords[t])
+                    if not adjacent:
+                        point_coords[t] = shift_point(point_coords[t-1], point_coords[t])
+                nine_coords.append(tuple(point_coords[-1]))
+    return nine_coords
+
+print("Part 1: ", len(set(run_calc(2))))
+print("Part 1: ", len(set(run_calc(10))))
+
+
